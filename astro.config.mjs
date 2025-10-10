176f7c4974e4f296c0bb498348290712a245d067
bin04 <arptable@hotmail.com>
2025-10-10 02:24:39 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://thedystopiafund.com',
+  integrations: [tailwind()],
   output: 'static'
 });
